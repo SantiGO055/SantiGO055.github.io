3df2d0652115855da5dbadf237441acc55f3941a
SantiGO055 <santigonzalez05@gmail.com>
2020-07-15 12:11:55 -0300

    .

--- js/forms.js
+++ js/forms.js
@@ -24,7 +24,7 @@ const vue = new Vue({
             .then(function(response) {
                 if(response.text === 'OK'){
                     alert('El correo se ha enviado de forma exitosa');
-                    clearForm(this.el);
+                    
                 }
                console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
             }, function(err) {
@@ -53,7 +53,8 @@ function clearForm(oForm) {
         case "message":
         case "subject":
             console.log(elements[i].value);
-          elements[i].value = "";
+            
+            elements[i].value = "";
           break;
   
         default:
