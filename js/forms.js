47fee8c6201d76b3b5a7fde30beb347f895c51b6
SantiGO055 <santigonzalez05@gmail.com>
2020-07-15 12:38:30 -0300

    .

--- js/forms.js
+++ js/forms.js
@@ -1,4 +1,39 @@
-
+(function(){
+    emailjs.init("user_qOzALb8QuxtW7Iy9808vc");
+ })();
+const vue = new Vue({
+    el: '#app',
+    data(){
+        return {
+            from_name: '',
+            from_email: '',
+            message: '',
+            subject: '',
+        }
+    },
+    methods: {
+        enviar(){
+            let data = {
+                from_name: this.from_name,
+                from_email: this.from_email,
+                message: this.message,
+                subject: this.subject,
+            };
+            
+            emailjs.send("gmail","form-contacto", data)
+            .then(function(response) {
+                if(response.text === 'OK'){
+                    alert('El correo se ha enviado de forma exitosa');
+                }
+               console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
+            }, function(err) {
+                console.log("casoandosdas");
+                alert('Ocurrió un problema al enviar el correo');
+               console.log("FAILED. error=", err);
+            });
+        }
+    }
+});
 
 function clearForm(oForm) {
 
